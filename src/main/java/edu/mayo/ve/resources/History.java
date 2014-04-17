d580acbfbe8f13223d3c804837b5ea1ce2a26ba7
Patrick Duffy <duffy.patrick@mayo.edu>
2014-04-17 20:20:50 +0000

    Added boundary case check in save()
    git-svn-id: https://bsisvn.mayo.edu/main/projects/m/mongo_svr/trunk@17723 8f329c14-d232-4f17-8684-7ee34322b8dc

--- src/main/java/edu/mayo/ve/resources/History.java
+++ src/main/java/edu/mayo/ve/resources/History.java
@@ -44,6 +44,7 @@ public class History {
 
     //CRUD
     //create
+    enum SaveAction {CREATE, UPDATE};
     @Path("save/")
     @POST
     @Produces("application/json")
@@ -56,34 +57,56 @@ public class History {
         String json = gson.toJson(filterHistory);
         BasicDBObject dbo = (BasicDBObject) JSON.parse(json);
 
+        // default action is to create a new mongo object
+        SaveAction action = SaveAction.CREATE;
+
+        // check if action should be to update existing object
         if (dbo.containsField("id") &&
                 dbo.getString("id").startsWith("f") &&
                 (dbo.getString("id").length()==41)) {
-            // existing FilterHistory, update it
 
-            // query collection based on workspace key and filter history id
-            BasicDBObject query = new BasicDBObject();
-            query.put("id", dbo.getString("id"));
-            query.put(Tokens.KEY, dbo.getString("key"));
+            // check if object exists in collection already
+            BasicDBObject existsQuery = new BasicDBObject();
+            existsQuery.put("id", dbo.getString("id"));
+            existsQuery.put(Tokens.KEY, dbo.getString("key"));
 
-            // update the object in collection
-            col.update(query, dbo);
-        }
-        else {
-            // brandnew FilterHistory, create it
-
-            //get a unique value and push it into the id field
-            WriteResult wr = col.save(dbo);
-            ObjectId id = dbo.getObjectId("_id");
-            //System.out.println("ID: " + id.toString());
-            String key = "f" + HashUtil.SHA1(HashUtil.randcat(id.toString())); //we need to add w so we don't get keys that are invalid such as 7foobar
-            //System.out.println("ID: " + key);
-            dbo.put("id", key);
-            BasicDBObject query = new BasicDBObject(); query.put("_id", id);
-            col.update(query, dbo);
+            DBCursor cursor = col.find(existsQuery);
+            try {
+                if (cursor.hasNext()) {
+                    // existing FilterHistory, update it
+                    action = SaveAction.UPDATE;
+                }
+            } finally {
+                cursor.close();
+            }
         }
 
-        return "{\"filterHistorySaved\":1}\n";
+        switch (action)
+        {
+            case CREATE: // brandnew FilterHistory, create it
+                //get a unique value and push it into the id field
+                WriteResult wr = col.save(dbo);
+                ObjectId id = dbo.getObjectId("_id");
+                //System.out.println("ID: " + id.toString());
+                String key = "f" + HashUtil.SHA1(HashUtil.randcat(id.toString())); //we need to add w so we don't get keys that are invalid such as 7foobar
+                //System.out.println("ID: " + key);
+                dbo.put("id", key);
+                BasicDBObject query = new BasicDBObject(); query.put("_id", id);
+                col.update(query, dbo);
+                break;
+            case UPDATE:
+                // query collection based on workspace key and filter history id
+                BasicDBObject updateQuery = new BasicDBObject();
+                updateQuery.put("id", dbo.getString("id"));
+                updateQuery.put(Tokens.KEY, dbo.getString("key"));
+
+                // update the object in collection
+                col.update(updateQuery, dbo);
+                break;
+        }
+
+        // return the saved/updated mongo object
+        return gson.toJson(dbo);
     }
 
     //CRUD
