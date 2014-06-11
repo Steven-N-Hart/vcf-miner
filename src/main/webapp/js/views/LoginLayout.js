bc0c437129a090a9a80034e7dc625eb4a7f91427
Patrick Duffy <duffy.patrick@mayo.edu>
2014-06-11 20:44:07 +0000

    login focus for username
    git-svn-id: https://bsisvn.mayo.edu/main/projects/m/mongo_view/trunk@18907 8f329c14-d232-4f17-8684-7ee34322b8dc

--- src/main/webapp/js/views/LoginLayout.js
+++ src/main/webapp/js/views/LoginLayout.js
@@ -9,6 +9,11 @@ LoginLayout = Backbone.Marionette.Layout.extend({
         "click .login" : "login"
     },
 
+    onShow: function() {
+        var usernameField = this.$el.find('#login_username_field');
+        usernameField.focus(); // request focus
+    },
+
     login: function() {
 
         var username = $("#login_username_field").val();
