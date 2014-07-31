f123fda3b6e6fc782f65d4b51d27371e6d04afab
Patrick Duffy <duffy.patrick@mayo.edu>
2014-07-31 20:46:46 +0000

    Added authorization check to getWorkspaceJSON()
    git-svn-id: https://bsisvn.mayo.edu/main/projects/m/mongo_svr/trunk@20026 8f329c14-d232-4f17-8684-7ee34322b8dc

--- src/main/java/edu/mayo/ve/resources/Queries.java
+++ src/main/java/edu/mayo/ve/resources/Queries.java
@@ -8,16 +8,15 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import javax.ws.rs.Path;
+
+import javax.ws.rs.*;
 
 import com.mongodb.WriteResult;
 import java.io.IOException;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
 
 import com.mongodb.Mongo;
 import com.mongodb.DB;
@@ -26,17 +25,15 @@ import com.mongodb.BasicDBObject;
 import com.mongodb.DBObject;
 import com.mongodb.DBCursor;
 import com.mongodb.util.JSON;
+import edu.mayo.util.SystemProperties;
 import edu.mayo.util.Tokens;
+import edu.mayo.ve.SecurityUserAppHelper;
 import edu.mayo.ve.util.BottomCleaner;
 import edu.mayo.util.MongoConnection;
 import java.net.UnknownHostException;
 import java.util.Iterator;
 import java.util.Set;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
-import javax.ws.rs.PathParam;
+
 import org.bson.types.ObjectId;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
@@ -50,12 +47,22 @@ import org.codehaus.jettison.json.JSONObject;
 
 @Path("/ve/q")
 public class Queries {
+
+    private SecurityUserAppHelper securityHelper;
+
+    public Queries() throws IOException {
+        securityHelper = new SecurityUserAppHelper(new SystemProperties());
+    }
+
      Mongo m = MongoConnection.getMongo();
      private static BottomCleaner bottomCleaner = new BottomCleaner();
      @GET
      @Path("/owner/list_workspaces/{user_id}")
      @Produces("application/json")
-     public String getWorkspaceJSON(@PathParam("user_id") String userID) {
+     public String getWorkspaceJSON(@PathParam("user_id") String userID, @HeaderParam("usertoken") String userToken) throws Exception {
+
+         final Set<String> authKeys = securityHelper.getAuthorizedWorkspaces(userToken);
+
          bottomCleaner.dropWorkspacesWOMetadata(); //deal with a strange bug where some workspaces exist even after they are deleted... if this is too slow, consider spawning up a workerpool to do it in the background
          //System.out.println("getWorkspaceJSON: " + userID);
          DB db = MongoConnection.getDB();
@@ -68,8 +75,12 @@ public class Queries {
          while(workspaces.hasNext()){
                 DBObject next = workspaces.next();
                 //System.out.println(next.toString());
-                docList.append(new Integer(counter).toString(), next);//next.get("_id"));
-                counter++;
+             String key = (String) next.get(Tokens.KEY);
+
+             if (authKeys.contains(key)) {
+                 docList.append(new Integer(counter).toString(), next);//next.get("_id"));
+                 counter++;
+             }
          }
          //System.err.println(docList.toString());
          return docList.toString() + "\n";
