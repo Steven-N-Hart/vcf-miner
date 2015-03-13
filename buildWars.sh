051682006e20aad5c2374440e566cf6373a7005c
Michael Meiners <meiners.michael1@mayo.edu>
2015-03-13 19:22:12 +0000

    buildWars.sh : changed to build individual war.  Stops on any error and prints a usage message
    git-svn-id: https://bsisvn.mayo.edu/main/projects/m/mongo_svr/trunk@25124 8f329c14-d232-4f17-8684-7ee34322b8dc

--- buildWars.sh
+++ buildWars.sh
@@ -1,36 +1,109 @@
 #!/bin/bash
 
-mongoUser=$1
-mongoPass=$2
-
-
-## If the mongo user and password are not specified, then show a warning saying that the integration and prod wars will not work
-if [ -z "$mongoUser" ]  ||  [ -z "$mongoPass" ] ; then
-  echo ""
-  echo ""
-  echo "***************************************"
-  echo "WARNING: No Mongo username and password specified, so the integration and production wars will not work"
-  echo "         (A Mongo username and password must be specified to connect to the Mongo instance)"
-  echo "USAGE:  ./buildWars.sh  <mongoUser>  <mongoPassword>"
-  echo "Ex:     ./buildWars.sh  TU00112  myPass123"
-  echo "Ex:     ./buildWars.sh  TU00112  'myPass123@#$^!'"
-  echo "***************************************"
-  echo ""
-  echo ""
-fi
-
+## Show all lines
+#set -x
 ## Exit on error
 set -e
 
+targetWar=$1
+mongoUser=$2
+mongoPass=$3
+
+
+echo "targetWar = $targetWar"
+
+function usage {
+  echo ""
+  echo ""
+  echo "***************************************"
+  echo "USAGE:  ./buildWars.sh  <targetWar>  [mongoUser]  [mongoPassword]"
+  echo "Where <targetWar> is one of:"
+  echo "    Default"
+  echo "    Biordev"
+  echo "    Rcftomdev1     (note: mongoUser and mongoPassword required)"
+  echo "    Rcftomprod1    (note: mongoUser and mongoPassword required)"
+  echo "    Virtualbox"
+  echo "    Windows"
+  echo ""
+  echo "War files will be built under the ./target/<targetWar> directory"
+  echo ""
+  echo "Ex:     ./buildWars.sh  Biordev"
+  echo "Ex:     ./buildWars.sh  Rcftomdev1  TU00112  'myPass123@#$^!'"
+  echo "***************************************"
+  echo ""
+  echo ""
+
+  exit 1
+}
+
+
+## If the user did not specify a war target 
+##    OR the user did not choose a valid build option
+##    OR they chose Rcftomdev1/Rcftomprod1 but did not specify a user & password
+## Then show usage and exit
+if [ -z "$targetWar" ] ; then 
+  echo "ERROR: No target war specified"
+  usage
+fi
+
+## Check if the user chose at least one of the target
+warOptions=(
+  'Default'
+  'Biordev'
+  'Rcftomdev1'
+  'Rcftomprod1'
+  'Virtualbox'
+  'Windows'
+  )
+## These sysprops files must match the warOptions above (same order)
+sysProps=(
+  'sys.properties'
+  'sys.properties.biordev'
+  'sys.properties.rcftomdev1'
+  'sys.properties.rcftomprod1'
+  'sys.properties.virtualbox'
+  'sys.properties.windows'
+  )
+isValidWarChosen="false"
+arraySize=${#warOptions[@]}
+idx=0
+for i in `seq 0 $((arraySize-1))`; do
+  if [ "${warOptions[i]}" == "$targetWar" ] ; then
+     isValidWarChosen="true"
+     idx=$i
+  fi
+done
+
+## Check for valid option
+if [ "true" == "$isValidWarChosen" ] ; then
+  echo "Valid war option chosen"
+else
+  echo "ERROR: Please choose a valid War file option"
+  usage
+fi
+
+## Check that a mongo user and password are specified when the Rcftomdev1 or Rcftomprod1 wars are chosen
+## (show usage if case doesn't match -we want to match on case because we're using the war in directory and file names)
+if [[ "$targetWar" == "Rcftomdev1" || "$targetWar" == "Rcftomprod1" ]] ; then
+  if [[ -z "$mongoUser" || -z "$mongoPass" ]] ; then
+    echo "Rcftomdev1 or Rcftomprod1 was specified as the target war; however, the Mongo username or password was not specified."
+    usage
+  fi
+else
+  echo "Dev or Prod not specified, so no username/password necessary"
+fi
+
+## Show error if not run from the project root dir
 if [ ! -d "target" ] ; then
   echo "Error: This script should be run from the project root directory, and the target subdirectory must already exist"
   exit 1;
 fi
 
-## Check if the war file exists first (so we can copy it and modify each of the instances created from it)
+## Check if the maven-built war file exists first (so we can copy it and modify each of the instances created from it)
 war=`ls target/mongo_svr*.war | tail -1`
 if [ ! -f "$war" ] ; then
   echo "Error: The war file must already be built:  target/mongo_svr*.war"
+  echo "Run this command to build the war:  mvn clean package" 
   exit 1;
 fi
 
@@ -40,38 +113,25 @@ mkdir -p target/Wars/Temp/WEB-INF/classes
 warVersion=v`ls $war | sed 's/target\///g' | sed 's/mongo_svr-//g' | sed 's/-SNAPSHOT//g' | sed 's/\.war//g'`
 touch target/Wars/$warVersion
 
-## Create a war file for each server we want to deploy to
-## Give it 3 parms.  For example:
-##   war 		= target/mongo_svr-2.0.2-SNAPSHOT
-##   targetDir 	= Biordev
-##   sysProps 	= sys.properties.biordev
-function crtWar() {
-  war=$1
-  warDir=$2
-  sysProps=$3
-  user=$4
-  pass=$5
+echo "Building war: target/Wars/$targetWar/mongo_svr.war"
+mkdir -p target/Wars/$targetWar
+cp $war target/Wars/$targetWar/mongo_svr.war
 
-  echo "Building war: target/Wars/$warDir/mongo_svr.war"
-  mkdir -p target/Wars/$warDir
-  cp $war target/Wars/$warDir/mongo_svr.war
 
-  ## Copy the appropriate sys.properties file to the Wars/Temp dir
-  cp src/main/resources/$sysProps  target/Wars/Temp/WEB-INF/classes/sys.properties
+## Copy the appropriate sys.properties file to the Wars/Temp dir
+sysProp=${sysProps[$idx]}
+cp src/main/resources/$sysProp  target/Wars/Temp/WEB-INF/classes/sys.properties
 
-  ## If the mongo username and password specified, then change the sys.properties to include it
-  if [ -n "$user" ]  &&  [ -n "$pass" ] ; then
-    props=`cat target/Wars/Temp/WEB-INF/classes/sys.properties | sed "s/mongo_user =.*$/mongo_user = $user/g"  |  sed "s/mongo_password =.*$/mongo_password = $pass/g"`
+## If the mongo username and password specified, then change the sys.properties to include it
+if [ -n "$mongoUser" ]  &&  [ -n "$mongoPass" ] ; then
+    props=`cat target/Wars/Temp/WEB-INF/classes/sys.properties | sed "s/mongo_user =.*$/mongo_user = $mongoUser/g"  |  sed "s/mongo_password =.*$/mongo_password = $mongoPass/g"`
     echo "$props" > target/Wars/Temp/WEB-INF/classes/sys.properties 
-  fi
+fi
 
-  ## Update the war with the correct sys.properties
-  jar uvf target/Wars/$warDir/mongo_svr.war  -C target/Wars/Temp  WEB-INF/classes/sys.properties
-}
+## Update the war with the correct sys.properties
+jar uvf target/Wars/$targetWar/mongo_svr.war  -C target/Wars/Temp  WEB-INF/classes/sys.properties
 
-crtWar  $war  Default     sys.properties
-crtWar  $war  Biordev     sys.properties.biordev
-crtWar  $war  Rcftomdev1  sys.properties.rcftomdev1   $mongoUser  $mongoPass
-crtWar  $war  Rcftomprod1 sys.properties.rcftomprod1  $mongoUser  $mongoPass
-crtWar  $war  Virtualbox  sys.properties.virtualbox
-crtWar  $war  Windows     sys.properties.windows
+
+echo ""
+echo "War file built: $targetWar"
+echo "DONE."
