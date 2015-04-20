ced29c928c119efcd8803ac96bb901233d5a5a00
Patrick Duffy <duffy.patrick@mayo.edu>
2015-04-20 18:58:48 +0000

    Fixed problem where duplicate SEARCH_SAVE events were being fired, causing issues downstream for the Analysis Save dialog box. FogBugz ID: 3956
    git-svn-id: https://bsisvn.mayo.edu/main/projects/m/mongo_view/trunk@25888 8f329c14-d232-4f17-8684-7ee34322b8dc

--- src/main/webapp/js/views/SearchSaveView.js
+++ src/main/webapp/js/views/SearchSaveView.js
@@ -2,10 +2,6 @@ SearchSaveView = Backbone.Marionette.ItemView.extend({
 
     template: '#search-save-template',
 
-    events: {
-        "click .searchSave" : "saveSearch"
-    },
-
     /**
      * Called when the view is first created
      *
@@ -15,20 +11,10 @@ SearchSaveView = Backbone.Marionette.ItemView.extend({
      */
     initialize: function(options)
     {
-        var self = this;
-
         // rebind so that options can be access in other functions
         this.options = options;
 
         this.listenTo(this.model, 'change', this.render);
-    },
-
-    /**
-     * Fire event to be handled by controller
-     * @param e
-     */
-    saveSearch: function(e) {
-        MongoApp.dispatcher.trigger(MongoApp.events.SEARCH_SAVE, MongoApp.search);
     }
 
 });
