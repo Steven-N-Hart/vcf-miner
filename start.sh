48b2e00e8f916ef8cdaddf85b57ddcc8893845ea
Steven-N-Hart <steven.n.hart@gmail.com>
2017-06-21 14:48:59 -0500

    updating start.sh

--- start.sh
+++ start.sh
@@ -2,9 +2,7 @@
 
 if [ -n "$NO_LDAP" ]
  then
- 	wget -O /usr/local/apache-tomcat-7.0.62/webapps/securityuserappNoLDAP.war https://github.com/Steven-N-Hart/vcf-miner/blob/master/securityuserappNoLDAP.war?raw=true
-else
-	wget -O /usr/local/apache-tomcat-7.0.62/webapps/securityuserapp.war https://github.com/Steven-N-Hart/vcf-miner/blob/master/securityuserapp.war?raw=true
+ 	wget -O /usr/local/apache-tomcat-7.0.62/webapps/securityuserapp.war https://github.com/Steven-N-Hart/vcf-miner/blob/master/securityuserappNoLDAP.war?raw=true
 fi
 /usr/local/apache-tomcat-7.0.62/bin/catalina.sh run &
 mongod --storageEngine wiredTiger 
