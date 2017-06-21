cf5a9d8fe6040041295ca2885400b2c97f022ed8
Steven-N-Hart <steven.n.hart@gmail.com>
2017-06-21 14:23:59 -0500

    trying new dockerfile parameters

--- start.sh
+++ start.sh
@@ -2,9 +2,9 @@
 
 if [ -n "$NO_LDAP" ]
  then
- 	cp /vcf-miner/securityuserappNoLDAP.war /usr/local/apache-tomcat-7.0.62/webapps/securityuserapp.war
+ 	wget -O /usr/local/apache-tomcat-7.0.62/webapps/securityuserappNoLDAP.war https://github.com/Steven-N-Hart/vcf-miner/blob/master/securityuserappNoLDAP.war?raw=true
 else
-	cp /vcf-miner/securityuserapp.war /usr/local/apache-tomcat-7.0.62/webapps/securityuserapp.war
+	wget -O /usr/local/apache-tomcat-7.0.62/webapps/securityuserapp.war https://github.com/Steven-N-Hart/vcf-miner/blob/master/securityuserapp.war?raw=true
 fi
 /usr/local/apache-tomcat-7.0.62/bin/catalina.sh run &
 mongod --storageEngine wiredTiger 
